99e78c60bc45276d2152d441f80b4201f8b2488f
Tuan Tran <langthamht047@gmail.com>
2022-09-17 20:38:48 -0700

    stream

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,5 +1,12 @@
-const {writeFileSync} = require('fs');
+const http = require('http');
+const fs =require('fs');
 
-for (let i= 0; i< 10000; i++) {
-    writeFileSync('./content/big.txt',`hello world ${i}\n`,{flag:'a'})
-}
+http.createServer(function(req,res) {
+    const fileStream = fs.createReadStream('./content/big.txt','utf8');
+    fileStream.on('open',() =>{
+        fileStream.pipe(res);
+    })
+    fileStream.on('error', (err) =>{
+        res.end(err);
+    })
+}).listen(5000)
